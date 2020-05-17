70a73875cf363fe78e8a81ff9acb637e2e892ec0
ikmalk <asyrafikmalwork@gmail.com>
2020-05-17 23:11:21 +0800

    First commit

---
+++ src/group/assignment/corona/Test.java
@@ -0,0 +1,15 @@
+package group.assignment.corona;
+
+public class Test {
+	/*
+	 * This is just a dummy class. 
+	 * To be deleted later
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Henlo World");
+
+	}
+
+}
